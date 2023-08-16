057c52d38b309b46e041071ce2a8d49c15fc7b85
samwhelp <samwhelp@users.noreply.github.com>
2023-08-16 23:27:46 +0800

    fix

--- port/part/path.sh
+++ port/part/path.sh
@@ -56,6 +56,7 @@ __prepend_path__ () {
 __prepend_path__ \
 	"${HOME}/bin" \
 	"${HOME}/.local/bin" \
+	"${__BASHRC_ENHANCE_DIR_PATH__}/tool/bin" \
 	"${__BASHRC_ENHANCE_DIR_PATH__}/port/tool/bin"
 
 
