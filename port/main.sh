348742f4f3409b3bcbe3e146d8749af49850b9d0
samwhelp <samwhelp@users.noreply.github.com>
2023-08-16 17:25:18 +0800

    fix

---
+++ port/main.sh
@@ -0,0 +1,169 @@
+
+
+################################################################################
+##
+## ~/.config/bashrc-enhance/port/main.sh
+##
+################################################################################
+
+
+
+
+################################################################################
+### Head: check mode
+##
+
+##
+## If not running interactively, don't do anything
+##
+[[ $- != *i* ]] && return
+
+
+##
+### Tail: check mode
+################################################################################
+
+
+
+
+################################################################################
+### Head: attribute / path
+##
+
+__BASHRC_ENHANCE_DIR_PATH__="${__BASHRC_ENHANCE_DIR_PATH__:=${HOME}/.config/bashrc-enhance}"
+
+##
+### Tail: attribute / path
+################################################################################
+
+
+
+
+################################################################################
+### Head: ignoreeof
+##
+
+##
+## * Search: [bash ctrl d](https://www.google.com/search?q=bash+ctrl+d)
+## * https://www.gnu.org/software/bash/manual/bash.html#index-end_002dof_002dfile-_0028usually-C_002dd_0029
+## * https://www.gnu.org/software/bash/manual/bash.html#index-IGNOREEOF
+##
+
+## disable `ctrl + d` to exit
+set -o ignoreeof
+
+
+##
+### Tail: ignoreeof
+################################################################################
+
+
+################################################################################
+### Head: path
+##
+
+##
+## Load ~/.config/bashrc-enhance/port/part/path.sh
+##
+
+[ -f "${__BASHRC_ENHANCE_DIR_PATH__}/port/part/path.sh" ] && . "${__BASHRC_ENHANCE_DIR_PATH__}/port/part/path.sh"
+
+
+##
+### Tail: path
+################################################################################
+
+
+################################################################################
+### Head: alias
+##
+
+
+alias ls='ls --color=auto'
+
+
+##
+## Load ~/.config/bashrc-enhance/port/part/alias.sh
+##
+
+[ -f "${__BASHRC_ENHANCE_DIR_PATH__}/port/part/alias.sh" ] && . "${__BASHRC_ENHANCE_DIR_PATH__}/port/part/alias.sh"
+
+
+##
+### Tail: alias
+################################################################################
+
+
+################################################################################
+### Head: prompt
+##
+
+
+#PS1='[\u@\h \W]\$ '
+
+#PS1='\u@\H [\t] (\w) \n $ '
+
+#PS1='\n\u@\H [\t] (\w) \n $ '
+
+
+#PS1='┌───────────────────>\n│ \u@\H [\t] (\w)\n└─> '
+
+#PS1='┌─────\n│ \u@\H [\t] (\w)\n└───── '
+
+#PS1='│\n│ \u@\H [\t] (\w)\n│ '
+
+#PS1='┌────────────────────\n│ \u@\H [\t] (\w)\n│ '
+
+PS1='┌─────\n│ \u@\H [\t] (\w)\n│ '
+
+
+##
+## Load ~/.config/bashrc-enhance/port/part/prompt
+##
+
+[ -f "${__BASHRC_ENHANCE_DIR_PATH__}/port/part/prompt.sh" ] && . "${__BASHRC_ENHANCE_DIR_PATH__}/port/part/prompt.sh"
+
+
+##
+### Tail: prompt
+################################################################################
+
+
+################################################################################
+### Head: bash-it
+##
+
+##
+## * https://github.com/Bash-it/bash-it
+##
+
+##
+## Load ~/.config/bashrc-enhance/port/part/bash_it_profile.sh
+##
+
+[ -f "${__BASHRC_ENHANCE_DIR_PATH__}/port/part/bash_it_profile.sh" ] && . "${__BASHRC_ENHANCE_DIR_PATH__}/port/part/bash_it_profile.sh"
+
+
+##
+### Tail: bash-it
+################################################################################
+
+
+################################################################################
+### Head: fzf
+##
+
+##
+## * https://wiki.archlinux.org/title/fzf
+##
+
+##
+## Load ~/.config/bashrc-enhance/port/part/fzf.bash
+##
+
+[ -f "${__BASHRC_ENHANCE_DIR_PATH__}/port/part/fzf.sh" ] && . "${__BASHRC_ENHANCE_DIR_PATH__}/port/part/fzf.sh"
+
+
+##
+### Tail: fzf
+################################################################################
